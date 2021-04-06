9f674d6fb3e8c8a23893b8854b9578382a89d5e2
Bruno Duarte <brunoarduarte@gmail.com>
2021-04-05 22:11:08 -0300

    Exercicio 6 e ajuste para funcoes

--- exercises/javascript-dom-eventos-e-web-storage_1/script.js
+++ exercises/javascript-dom-eventos-e-web-storage_1/script.js
@@ -1,19 +1,43 @@
 //Exercício 1
-document.getElementsByTagName('p')[0].innerText = "Full stack developer and UI/UX Designer";
+function changeTxt () {
+let setTxt = document.getElementsByTagName('p')[0]
+setTxt.innerText = "Full stack developer and UI/UX Designer Hired";
+}
+changeTxt ();
 
 //Exercício 2
+function changeBkColor () {
 let bkground = document.getElementsByClassName('main-content')[0];
-
 bkground.style.backgroundColor = 'rgb(76,164,109)';
+}
+changeBkColor ();
 
 //Exercício 3
+function changeBkColor2 () {
 let bkground2 = document.getElementsByClassName('center-content')[0];
-
 bkground2.style.backgroundColor = 'white';
+}
+changeBkColor2 ();
 
 //Exercício 4
-document.getElementsByClassName('title')[0].innerText = 'Exercício 5.1 - JavaScript';
+function correctTxt () {
+let oldTxt = document.getElementsByClassName('title')[0]
+oldTxt.innerText = 'Exercício 5.1 - JavaScript';
+}
+correctTxt ();
 
 //Exercício 5
-document.getElementsByTagName('p')[0].style.textTransform = 'uppercase';
+function toUpperCase () {
+let baseTxt = document.getElementsByTagName('p')[0]
+baseTxt.style.textTransform = 'uppercase';
+}
+toUpperCase ();
 
+//Exercício 6
+function showParagraphs () {
+  let par = document.getElementsByTagName('p');
+  for (i = 0; i < par.length; i += 1) {
+    console.log(par[i].innerHTML);
+  }
+}
+showParagraphs ();
