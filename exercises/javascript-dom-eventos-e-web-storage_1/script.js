1a3c63f338c2e270a7da197fb703f9db5db174a7
Bruno Duarte <brunoarduarte@gmail.com>
2021-04-05 21:41:36 -0300

    Exercicio 4

--- exercises/javascript-dom-eventos-e-web-storage_1/script.js
+++ exercises/javascript-dom-eventos-e-web-storage_1/script.js
@@ -1,10 +1,14 @@
 document.getElementsByTagName('p')[0].innerText = "Full stack developer and UI/UX Designer";
 
+
 let bkground = document.getElementsByClassName('main-content')[0];
 
 bkground.style.backgroundColor = 'rgb(76,164,109)';
+
 
 let bkground2 = document.getElementsByClassName('center-content')[0];
 
 bkground2.style.backgroundColor = 'white';
 
+
+document.getElementsByClassName('title')[0].innerText = 'Exercício 5.1 - JavaScript';
