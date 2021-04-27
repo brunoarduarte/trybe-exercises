ad7e8a13a1d55c1256e2d4bc73eb46b02a5cf161
Bruno Duarte <brunoarduarte@gmail.com>
2021-04-27 11:11:10 -0300

    Exercicio bloco 7.1 - Parte 2 - Exercicio 2

--- exercises/introducao-a-javascript-es6-e-testes-unitarios_1/script.js
+++ exercises/introducao-a-javascript-es6-e-testes-unitarios_1/script.js
@@ -1,21 +1,31 @@
 function testingScope(escopo) {
-    if (escopo === true) {
-      let ifScope = 'Não devo ser utilizada fora do meu escopo (if)';
-      ifScope = ifScope + ' ótimo, fui utilizada no escopo !';
-      console.log(ifScope);
-    } else {
-      let elseScope = 'Não devo ser utilizada fora meu escopo (else)';
-      console.log(elseScope);
-    }
+  if (escopo === true) {
+    let ifScope = 'Não devo ser utilizada fora do meu escopo (if)';
+    ifScope = ifScope + ' ótimo, fui utilizada no escopo !';
+    console.log(ifScope);
+  } else {
+    let elseScope = 'Não devo ser utilizada fora meu escopo (else)';
+    console.log(elseScope);
   }
+}
 
-  testingScope(true);
+testingScope(true);
 
-  const oddsAndEvens = [13, 3, 4, 10, 7, 2];
+const oddsAndEvens = [13, 3, 4, 10, 7, 2];
   
-  function sortingArray() {
-      return `Os números ${oddsAndEvens.sort(function(a, b){return a - b})} se encontram ordenados de forma crescente!.`
-  }
+function sortingArray() {
+  return `Os números ${oddsAndEvens.sort(function(a, b){return a - b})} se encontram ordenados de forma crescente!.`
+}
 
   console.log(sortingArray());
-  
+
+let arrayOfNumbers = [];
+
+function factorial (n) {
+  if (n == 0 || n == 1)
+    return 1;
+  if (arrayOfNumbers[n] > 0)
+    return arrayOfNumbers[n];
+  return arrayOfNumbers[n] = factorial(n-1) * n;
+}
+console.log(factorial(4));
