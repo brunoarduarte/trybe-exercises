3fb327a6aec16804367a6e0af5f2281b517333dd
Bruno Duarte <brunoarduarte@gmail.com>
2021-05-03 09:56:11 -0300

    Exercicio 1 - parte 1 Finalizado

--- exercises/higher-order-functions-do-javascript-es6_1/exercício1-parte1.js
+++ exercises/higher-order-functions-do-javascript-es6_1/exercício1-parte1.js
@@ -1,9 +1,15 @@
-const newEmployees = () => {
-    const employees = {
-      id1: employeesGen('Pedro Guerra'),// Nome: Pedro Guerra -> Chame sua função passando o nome Pedro Guerra como parâmetro
-      id2: employeesGen('Luiza Drumond'),// Nome: Luiza Drumond -> Chame sua função passando o nome Luiza Drumond como parâmetro
-      id3: employeesGen('Carla Paiva')// Nome: Carla Paiva -> Chame sua função passando o nome Carla Paiva como parâmetro
-    }
-    return employees;
-  };
-  
+const emailGenerator = (fullName) => {
+  const email = fullName.toLowerCase().split(' ').join('_');
+  return {fullName, email: `${email}@trybe.com`};
+};
+
+const newEmployees = (block) => {
+  const employees = {
+    id1: block('Pedro Guerra'),// Nome: Pedro Guerra -> Chame sua função passando o nome Pedro Guerra como parâmetro
+    id2: block('Luiza Drumond'),// Nome: Luiza Drumond -> Chame sua função passando o nome Luiza Drumond como parâmetro
+    id3: block('Carla Paiva')// Nome: Carla Paiva -> Chame sua função passando o nome Carla Paiva como parâmetro
+  }
+  return employees;
+};
+
+console.log(newEmployees(emailGenerator));
